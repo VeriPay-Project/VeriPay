d78cbbe9eb8f2035c60ba75abfb678fdedf44ebb
Aryan <164097681+this-isaryan@users.noreply.github.com>
2026-03-29 16:56:23 -0400

    Delete ai_pipeline directory

--- ai_pipeline/advanced/pipeline_layoutlm.py
+++
@@ -1,9 +0,0 @@
-from advanced.layoutlm_features import extract_layoutlm_embedding
-
-
-def process_invoice_layoutlm(pdf_path):
-    """
-    Processes an invoice using LayoutLMv3 embeddings
-    instead of handcrafted features.
-    """
-    return extract_layoutlm_embedding(pdf_path)
